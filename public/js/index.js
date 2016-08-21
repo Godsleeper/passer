bcb5b799fa58084085c8ce25ca2f50a868960b0a
Godsleeper <nxtw@qq.com>
2016-08-21 19:20:28 +0800

    version 0.0.14

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 $(function(){
+	//标题栏上移
 	$(".triangel").click(function(){
 		$(".topbar").css({height:60})
 	});
@@ -7,4 +8,38 @@ $(function(){
 		$(".topbar").css({height:480});
 		return false;
 	})
+	//图片轮播
+	$(".listbtn").click(function(event){
+		var target = event.target;
+		switch(target.id){
+			case "imgbtn1":
+				clear();	
+				$(target).addClass("active");
+				move(target.id);
+				break;
+			case "imgbtn2":
+				clear();
+				$(target).addClass("active");
+				move(target.id);
+				break;
+			case "imgbtn3":
+				clear();
+				$(target).addClass("active");
+				move(target.id);
+				break;
+		}
+	})
+	var clear = function(){
+		$(".topbar_bg").removeClass('cssmove');
+		$(".listbtn").children().removeClass("active");
+
+	}
+	var move = function(idx){
+		var str = idx;
+		var id = parseInt(str.substr(str.length-1));
+		var offset = (id-1)*-1349+'px';
+		$(".topbar_bg").addClass('cssmove').css({
+			left:offset});
+	}
+
 })
